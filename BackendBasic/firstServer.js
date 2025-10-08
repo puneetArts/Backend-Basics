268d9bba04915ba244e32965e0cb3cc5cdf7f448
unknown <puneetsingh91100@gmail.com>
2025-10-08 20:08:42 +0530

    firstServer_tutorial-2

--- BackendBasic/firstServer.js
+++ BackendBasic/firstServer.js
@@ -1,5 +1,5 @@
 const http=require ('http');
-
+const fs= require('fs');
 // function requestListener(req,res){
 //   console.log(req);
 // }
@@ -8,6 +8,37 @@ const http=require ('http');
 
 const server=http.createServer(function requestListener(req,res){
   console.log(req);
+
+  if(req.url==='/'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Complete Coding</title></head>');
+    res.write('<body><h1>Enter Your Details:</h1>');
+    res.write('<form action="/submit-details" method="POST">');
+    res.write('<input type="text" name="username" placeholder="Enter your name"><br>');
+    res.write('<label for="male">Male</label>')
+    res.write('<input type="radio" id="male" name="gender" value="male" />')
+    res.write('<label for="female">Female</label>')
+    res.write('<input type="radio" id="female" name="gender" value="female" />')
+    res.write('<br><input type="submit" value="Submit">');
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
+  return res.end();
+
+  }else if(req.url.toLocaleLowerCase()==='/submit-details' && req.method=='POST'){
+   
+    fs.writeFileSync('user.txt','puneet');
+    res.statusCode=302;
+    res.setHeader('Location', '/');
+
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Hello ji</title></head>');
+  res.write('<body><h1>learning Node</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 
